ca23fe1479641a222afc75472963226db5737301
DedSecer <ded5ec@icloud.com>
2026-04-03 20:18:41 +0800

    add motion server for tienkung robot

--- run_motion_server.sh
+++ run_motion_server.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 script_dir=$(dirname $(realpath $0))
-# motion_file="${script_dir}/assets/example_motions/0807_yanjie_walk_005.pkl"
 motion_file="${script_dir}/assets/example_motions/0807_yanjie_walk_001.pkl"
 
+# Usage:
+#   bash run_motion_server.sh [robot] [motion_file]
+# Examples:
+#   bash run_motion_server.sh unitree_g1_with_hands
+#   bash run_motion_server.sh tienkung
+#   bash run_motion_server.sh tienkung /abs/path/to/your_motion.pkl
+
+robot=${1:-unitree_g1_with_hands}
+
+# Default motion file can be overridden by arg2.
+
+default_motion_file="${script_dir}/assets/example_motions/0807_yanjie_walk_001.pkl"
+
+motion_file=${2:-$default_motion_file}
 
 # Change to deploy_real directory
 cd deploy_real
@@ -17,7 +30,7 @@ redis_ip="localhost"
 # Run the motion server
 python server_motion_lib.py \
     --motion_file ${motion_file} \
-    --robot unitree_g1_with_hands \
+    --robot ${robot} \
     --vis \
     --redis_ip ${redis_ip}
     # --send_start_frame_as_end_frame \
